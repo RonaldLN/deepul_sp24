e37f729353625f40ae6b7991c64bbe48adbe55f3
Ronald_Luo <1239751758@qq.com>
2026-05-02 19:27:23 +0800

    Finish hw3 q3b: ViT-VQGAN

---
+++ models/vit.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+from .transformer import *
+
+
+# copy from cs231n
+class PatchEmbedding(nn.Module):
+  def __init__(self, img_size, patch_size, in_channels=3, embed_dim=256):
+    super().__init__()
+    self.img_size = img_size
+    self.patch_size = patch_size
+    self.embed_dim = embed_dim
+
+    assert img_size % patch_size == 0, "Image dimensions must be divisible by the patch size."
+
+    self.num_patches = (img_size // patch_size) ** 2  # H/P * W/P
+    self.patch_dim = patch_size ** 2 * in_channels  # P * P * C
+
+    self.proj = nn.Linear(self.patch_dim, embed_dim)
+
+  def forward(self, x):
+    N, C, H, W = x.shape
+    assert H == self.img_size and W == self.img_size, \
+      f"Expected image size ({self.img_size}, {self.img_size}), but got ({H}, {W})"
+
+    P = self.patch_size
+    x = x.reshape(N, C, H//P, P, W//P, P)
+    x = x.permute(0, 2, 4, 1, 3, 5)  # (N, H/P, W/P, C, P, P)
+    x = x.reshape(N, self.num_patches, self.patch_dim)  # (N, H/P * W/P, C*P*P)
+    x = self.proj(x)  # (N, H/P * W/P, D)
+
+    return x
+
+
+class PatchToImage(nn.Module):
+  def __init__(self, img_size, patch_size, in_channels=3, embed_dim=256):
+    super().__init__()
+    self.img_size = img_size
+    self.patch_size = patch_size
+    self.in_channels = in_channels
+    self.embed_dim = embed_dim
+
+    self.num_patches = (img_size // patch_size) ** 2  # H/P * W/P
+    self.patch_dim = patch_size ** 2 * in_channels  # P * P * C
+
+    self.proj = nn.Linear(embed_dim, self.patch_dim)
+
+  def forward(self, x):
+    C, H, W = self.in_channels, self.img_size, self.img_size
+    P = self.patch_size
+    x = self.proj(x)  # (N, H/P * W/P, C*P*P)
+    x = x.reshape(-1, H//P, W//P, C, P, P)
+    x = x.permute(0, 3, 1, 4, 2, 5)  # (N, C, H/P, P, W/P, P)
+    x = x.reshape(-1, C, H, W)
+    return x
+
+
+class ViTEncoderLayer(CausalTransformerDecoderLayer):
+  pass
+
+
+class ViTEncoder(CausalTransformerDecoder):
+  pass
+
+
+# largely copy from cs231n
+class ViTVQGANEncoder(nn.Module):
+  def __init__(self, img_size=32, patch_size=4, in_channels=3, embed_dim=256,
+               num_layers=4, num_heads=8, dim_feedforward=2*256, max_length=8*8, dropout=0.1):
+    super().__init__()
+    self.patch_embed = PatchEmbedding(img_size, patch_size, in_channels, embed_dim)
+    self.positional_encoding = nn.Parameter(torch.randn(max_length, embed_dim))
+
+    encoder_layer = ViTEncoderLayer(embed_dim, num_heads, dim_feedforward, dropout)
+    self.transformer = ViTEncoder(encoder_layer, num_layers)
+
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+    if isinstance(module, (nn.Linear, nn.Embedding)):
+      module.weight.data.normal_(mean=0.0, std=0.02)
+      if isinstance(module, nn.Linear) and module.bias is not None:
+        module.bias.data.zero_()
+      elif isinstance(module, nn.LayerNorm):
+        module.bias.data.zero_()
+        module.weight.data.fill_(1.0)
+
+  def forward(self, x):
+    x = self.patch_embed(x)  # (N, H/P * W/P, D)
+    num_patches = x.shape[1]
+    x = x + self.positional_encoding[:num_patches]
+    x = self.transformer(x)
+    return x
+
+
+class ViTVQGANDecoder(nn.Module):
+  def __init__(self, img_size=32, patch_size=4, in_channels=3, embed_dim=256,
+               num_layers=4, num_heads=8, dim_feedforward=2*256, max_length=8*8, dropout=0.1):
+    super().__init__()
+    self.positional_encoding = nn.Parameter(torch.randn(max_length, embed_dim))
+
+    encoder_layer = ViTEncoderLayer(embed_dim, num_heads, dim_feedforward, dropout)
+    self.transformer = ViTEncoder(encoder_layer, num_layers)
+
+    self.patch_to_image = PatchToImage(img_size, patch_size, in_channels, embed_dim)
+
+    self.apply(self._init_weights)
+
+  def _init_weights(self, module):
+    if isinstance(module, (nn.Linear, nn.Embedding)):
+      module.weight.data.normal_(mean=0.0, std=0.02)
+      if isinstance(module, nn.Linear) and module.bias is not None:
+        module.bias.data.zero_()
+      elif isinstance(module, nn.LayerNorm):
+        module.bias.data.zero_()
+        module.weight.data.fill_(1.0)
+
+  def forward(self, x):
+    num_patches = x.shape[1]
+    x = x + self.positional_encoding[:num_patches]
+    x = self.transformer(x)  # (N, H/P * W/P, D)
+    x = self.patch_to_image(x)  # (N, C, H, W)
+    return x
--- models/vqgan.py
+++ models/vqgan.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.utils.parametrizations import spectral_norm
 from deepul.hw3_utils.lpips import LPIPS
 from .vqvae import *
 from .wgan_gp import *
+from .vit import *
 
 
 class VQGANVQVAE(VQVAE):
@@ -77,3 +79,83 @@ class VQGAN(nn.Module):
 
   def reconstruct(self, x):
     return self.vqvae.reconstruct(x)
+
+
+class ViTVQGANVQVAE(VQGANVQVAE):
+  def __init__(self, k, beta=0.25):
+    nn.Module.__init__(self)
+    self.beta = beta
+
+    self.encoder = ViTVQGANEncoder(img_size=32, patch_size=4, in_channels=3, embed_dim=256,
+                                   num_layers=4, num_heads=8, dim_feedforward=2*256, max_length=8*8, dropout=0.1)
+    self.decoder = ViTVQGANDecoder(img_size=32, patch_size=4, in_channels=3, embed_dim=256,
+                                   num_layers=4, num_heads=8, dim_feedforward=2*256, max_length=8*8, dropout=0.1)
+    # Initialize codebook with uniform(-1/K, 1/K)
+    self.codebook = nn.Parameter(torch.empty(k, 256).uniform_(-1/k, 1/k))
+
+  def _quantize(self, x):
+    z_e = self.encoder(x)  # (N, H/P * W/P, D)
+    N, _, D = z_e.shape
+    z_e_flatten = z_e.view(-1, 1, D)  # (N * H/P * W/P, 1, 256)
+
+    squared_distances = (z_e_flatten - self.codebook) ** 2  # (N', 1, 256) - (K, 256) -> (N', K, 256)
+    squared_distances = torch.sum(squared_distances, dim=-1)  # (N', K)
+
+    indices = torch.argmin(squared_distances, dim=1)  # (N',)
+    indices = indices.view(N, -1)  # (N, H/P * W/P)
+    return indices, z_e
+
+  def loss(self, x):
+    indices, z_e = self._quantize(x)
+    z_q = self.codebook[indices]  # (N, H/P * W/P) -> (N, H/P * W/P, D)
+
+    # straight-through estimator (given)
+    z_q_st = (z_q - z_e).detach() + z_e
+    recon_x = self.decoder(z_q_st)
+
+    recon_loss = F.mse_loss(recon_x, x)
+    vq_loss = F.mse_loss(z_q, z_e.detach())
+    commitment_loss = F.mse_loss(z_e, z_q.detach())
+
+    loss = recon_loss + vq_loss + self.beta * commitment_loss
+
+    return loss, recon_loss, recon_x
+
+  def decode(self, indices):
+    z_q = self.codebook[indices]  # (N, H/P * W/P) -> (N, H/P * W/P, D)
+    decoded_img = self.decoder(z_q)
+    return decoded_img
+
+
+class ViTVQGAN(VQGAN):
+  def __init__(self, k, n_filters):
+    nn.Module.__init__(self)
+
+    self.vqvae = ViTVQGANVQVAE(k)
+    self.discriminator = nn.Sequential(
+      ResnetBlockDown(3, n_filters=n_filters),
+      ResnetBlockDown(128, n_filters=n_filters),
+      ResBlock(n_filters, n_filters=n_filters),
+      ResBlock(n_filters, n_filters=n_filters),
+      nn.LeakyReLU(),
+      GlobalSumPooling(),  # global sum pooling
+      nn.Linear(128, 1)
+    )
+    # apply spectral norm to weight matrices
+    for m in self.discriminator.modules():
+      if isinstance(m, (nn.Linear, nn.Conv2d, nn.ConvTranspose2d)):
+        spectral_norm(m)
+
+    self._l_pips = LPIPS()
+
+  def discriminate(self, x):
+    return self.discriminator(x)  # (N, C, H, W) -> (N, 1)
+  
+  def vqvae_loss(self, x, recon_x, vq_loss, l2_loss):
+    logits_fake = self.discriminate(recon_x)  # D(x_hat) (logits)
+    # min log(1-D(x_hat)) => max log(D(x_hat)) => min -log(D(x_hat))
+    gan_loss = F.binary_cross_entropy_with_logits(logits_fake, torch.ones_like(logits_fake))  # -log(D(x_hat))
+    perceptual_loss = self.l_pips(x, recon_x)
+    l1_loss = F.l1_loss(x, recon_x)
+    vqvae_loss = vq_loss + 0.1 * gan_loss + 0.5 * perceptual_loss + l2_loss + 0.1 * l1_loss
+    return vqvae_loss, perceptual_loss, l2_loss
